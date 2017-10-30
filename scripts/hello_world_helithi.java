5c86e374fb0599f9ecf761c66eb2d094711b1980
helithiga <33217879+helithiga@users.noreply.github.com>
2017-10-30 17:03:43 +0530

    create hello_world_helithi.java

---
+++ scripts/hello_world_helithi.java
@@ -0,0 +1,14 @@
+/ LANGUAGE: Java
+// ENV: Node.java
+// AUTHOR: Helithi
+// GITHUB: https://github.com/helithiga
+
+
+
+
+
+class hello_world_helithi{
+  public static void main(String[]args){
+    System.out.println("Hello, World");
+  }
+}
